de3c565f7dde6b78737913227907d276f7cdf169
sferrka <enikitina.phuket@gmail.com>
2020-06-14 18:45:06 +0800

    Added: docs (prod) folder for github pages

---
+++ docs/precache-manifest.fc55ef1d0413b28c3498f3727b4a7eea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82644be6443c01acc3a948506b93f427",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db2baa02f19fbdadb02d",
+    "url": "/static/css/2.81187608.chunk.css"
+  },
+  {
+    "revision": "83aa4ebc7e42506ee86d",
+    "url": "/static/css/main.23743079.chunk.css"
+  },
+  {
+    "revision": "db2baa02f19fbdadb02d",
+    "url": "/static/js/2.f9ef552f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f9ef552f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83aa4ebc7e42506ee86d",
+    "url": "/static/js/main.e7d8da51.chunk.js"
+  },
+  {
+    "revision": "edd87c5a108f0863d55d",
+    "url": "/static/js/runtime-main.1eab38db.js"
+  }
+]);
